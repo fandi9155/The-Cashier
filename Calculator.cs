e00f76213c4f708338ced3af621285cdd78680fd
yusuf <mukhamad07@students.amikom.ac.id>
2020-10-21 10:27:14 +0700

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Windows.Documents;
+
+namespace The_Cashier
+{
+    class Calculator
+    {
+        private List<Item> listItem;
+        private double total = 0;
+
+        public Calculator()
+        {
+            this.listItem = new List<Item>();
+        }
+
+        public void addItem(Item item)
+        {
+            this.listItem.Add(item);
+            this.total += item.getSubtotal();
+
+        }
+
+        public double getTotal()
+        {
+            return total;
+        }
+        public List<Item> getListItem()
+        {
+            return listItem;
+        }     
+        
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Interop;
+
+namespace The_Cashier
+{
+    class Item
+    {
+        private int id;
+        public string title { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+        private int v;
+        private string titl;
+        private object quantityype;
+
+        public Item (int id, string title, int quantity, double price, double subtotal, string type)
+        {
+            this.id = id;
+            this.title = title;
+            this.quantity = quantity;
+            this.price = price;
+            this.subtotal = subtotal;
+            this.type = type;
+
+            
+        }
+
+        public Item(int v, string titl, object quantityype, double price)
+        {
+            this.v = v;
+            this.titl = titl;
+            this.quantityype = quantityype;
+            this.price = price;
+        }
+
+        public Item(int v, string titl, object quantityype, double price, string type) : this(v, titl, quantityype, price)
+        {
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }    
+
+        public int getQuantity()
+        {
+            return quantity;
+        }
+        public string getType()
+        {
+            return type;
+
+        }
+
+        public double getPrice()
+        {
+            return price;
+
+        }
+
+        public double getSubtotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }
+
+    }
+}
